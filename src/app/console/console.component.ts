83778f325df4738738c4d5dd452239405a1bc20b
Ryu Master <assignators@gmail.com>
2018-06-01 12:56:16 +0300

    Console gets debug lot toggler and better formatting

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -21,6 +21,7 @@ export class ConsoleComponent  {
 	private consoleTextHolder:string = "";
 	public consoleText:string = "";
     public carretOn:boolean = false;
+	public viewDebugON:boolean = false;
 	
 	constructor(private globalService:GlobalService) 
 	{
@@ -37,37 +38,97 @@ export class ConsoleComponent  {
 	  }
 	}
 	
-
-	
-	carretBlink()
+    toggleDebugLog()
 	{
-		if(!this.carretOn)
+		this.viewDebugON = !this.viewDebugON;
+		let _that = this;
+		if(this.viewDebugON)
 		{
-			this.consoleText = this.consoleTextHolder + "_";
+				var userAgent = navigator.userAgent.toLowerCase();
+				if (userAgent.indexOf(' electron/') == -1) 
+				{
+				   return;
+				}	  
+	  
+	  
+
+				const path = window.require('path');
+				let basepath = window.require('electron').remote.app.getPath('appData');
+				let filename = path.join(basepath, './Chimaera/debug.log');
+				const fs = window.require('fs');
+				
+
+				
+				if (fs.existsSync(filename)) 
+				{
+				}
+				else
+				{
+					 filename = path.join(basepath, './Chimaera/testnet/debug.log');
+					
+					if (!fs.existsSync(filename)) 
+					{
+						return;
+					}
+					
+				}
+				
+				
+				let contents = "";
+				fs.readFile(filename, 'utf8', function(err, data) 
+				{
+					if (err) throw err;
+					contents = data;
+					_that.consoleText = contents;
+					
+				}
+				
+					
 		}
 		else
 		{
-			this.consoleText = this.consoleTextHolder;
-			this.consoleText.replace(/\\n/g, String.fromCharCode(13, 10));
+			
+			this.carretBlink();
+			
 		}
 		
-		this.carretOn = !this.carretOn;
+	}
+	
+	carretBlink()
+	{
+		if(this.viewDebugON == false)
+		{
+			if(!this.carretOn)
+			{
+				this.consoleText = this.consoleTextHolder + "_";
+				this.consoleText =  this.consoleText.replace(/\\n/g, String.fromCharCode(13, 10));
+			}
+			else
+			{
+				this.consoleText = this.consoleTextHolder;
+				this.consoleText =  this.consoleText.replace(/\\n/g, String.fromCharCode(13, 10));
+			}
+			
+			this.carretOn = !this.carretOn;
+		}
 	}
 	
 	async sendConsoleCommand()
 	{
-		var cmd = this.command;
-		this.command = "";
-		var result = await this.globalService.consoleCommand(cmd);
+		let cmd = this.command;
 		
+		let result = await this.globalService.consoleCommand(cmd);
+		let str = JSON.stringify(result, null, 2);
 		
-		this.consoleTextHolder += result;
-		this.consoleTextHolder += "\n";
+		this.consoleTextHolder += cmd;
+		this.consoleTextHolder += "\n";;
+		this.consoleTextHolder += str;
+		this.consoleTextHolder += "\n\n";
 		
 		this.consoleText = this.consoleTextHolder;
-		this.consoleText.replace(/\\n/g, String.fromCharCode(13, 10));
+		this.consoleText =  this.consoleText.replace(/\\n/g, String.fromCharCode(13, 10));
 		
-		
+		this.command = "";
 	}
 
 }
