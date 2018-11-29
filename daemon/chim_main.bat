a563b1f7e3b4f8c07a11a4538c1d736a23318b57
Ryu Master <assignators@gmail.com>
2018-11-29 17:50:41 +0200

    add  -zmqgameblocks=tcp://127.0.0.1:28332

--- daemon/chim_main.bat
+++ daemon/chim_main.bat
@@ -4,11 +4,11 @@ cls
 if exist "!AppData!\Xaya\appdata.orv" (
     set /p DataDirPath=<!AppData!\Xaya\appdata.orv
 	tasklist|find /i "xayad.exe" >NUL
-	if errorlevel 1 xayad.exe -wallet=vault.dat  -wallet=game.dat -server -rpcallowip=127.0.0.1 -datadir=!DataDirPath! -zmqpubhashtx=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubrawblock=tcp://127.0.0.1:28332 -zmqpubrawtx=tcp://127.0.0.1:28332
+	if errorlevel 1 xayad.exe -wallet=vault.dat  -wallet=game.dat -server -rpcallowip=127.0.0.1 -datadir=!DataDirPath! -zmqpubhashtx=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubrawblock=tcp://127.0.0.1:28332 -zmqpubrawtx=tcp://127.0.0.1:28332 -zmqgameblocks=tcp://127.0.0.1:28332
 	goto :eof	
 ) else (
 	tasklist|find /i "xayad.exe" >NUL
-	if errorlevel 1 xayad.exe -wallet=vault.dat  -wallet=game.dat -server -rpcallowip=127.0.0.1 -zmqpubhashtx=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubrawblock=tcp://127.0.0.1:28332 -zmqpubrawtx=tcp://127.0.0.1:28332
+	if errorlevel 1 xayad.exe -wallet=vault.dat  -wallet=game.dat -server -rpcallowip=127.0.0.1 -zmqpubhashtx=tcp://127.0.0.1:28332 -zmqpubhashblock=tcp://127.0.0.1:28332 -zmqpubrawblock=tcp://127.0.0.1:28332 -zmqpubrawtx=tcp://127.0.0.1:28332 -zmqgameblocks=tcp://127.0.0.1:28332
 	goto :eof
 )
 
